754ff7498928612e015e69c63bb0ea822de7c9e8
Pröstl Andren Filip <Filip.Proestl-Andren@ait.ac.at>
2022-01-12 09:24:51 +0100

    updated script

--- get_latest_xml2md.sh
+++ get_latest_xml2md.sh
@@ -1,18 +1,18 @@
 set -o nounset
 set -o errexit
 
-latest_url=$(wget https://github.com/smart-grid-use-cases/xml2md/releases/latest --max-redirect=0 2>&1 | grep Location)
+latest_url=$(wget https://github.com/ERIGrid2/test-case-descriptions-excel2md/releases/latest --max-redirect=0 2>&1 | grep Location)
 echo "Latest URL: $latest_url"
-prefix="Location: https://github.com/smart-grid-use-cases/xml2md/releases/tag/"
+prefix="Location: https://github.com/ERIGrid2/test-case-descriptions-excel2md/releases/tag/"
 suffix=" [following]"
 tail=${latest_url#"$prefix"}
 release_version=${tail%"$suffix"}
 echo "Release version: $release_version"
-wget https://github.com/smart-grid-use-cases/xml2md/releases/download/${release_version}/Bridge.zip
-unzip Bridge.zip -d content/en/docs/
-for dir in $(find content/en/docs/Bridge/ -mindepth 1 -maxdepth 1 -type d);
+wget https://github.com/ERIGrid2/test-case-descriptions-excel2md/releases/download/${release_version}/erigrid2-test-cases.zip
+unzip erigrid2-test-cases.zip -d content/en/docs/
+for dir in $(find content/en/docs/excel-test-cases/ -mindepth 1 -maxdepth 1 -type d);
 do
   cp -a $dir content/en/docs/UseCases
 done
 rm Bridge.zip
-rm -rf content/en/docs/Bridge
+rm -rf content/en/docs/excel-test-cases
