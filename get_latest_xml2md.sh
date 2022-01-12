3be76c270f74d3c02e74fc6ce76024460a499510
Pröstl Andren Filip <Filip.Proestl-Andren@ait.ac.at>
2022-01-12 11:20:55 +0100

    updated workflow

--- get_latest_xml2md.sh
+++ get_latest_xml2md.sh
@@ -10,9 +10,9 @@ release_version=${tail%"$suffix"}
 echo "Release version: $release_version"
 wget https://github.com/ERIGrid2/test-case-descriptions-excel2md/releases/download/${release_version}/erigrid2-test-cases.zip
 unzip erigrid2-test-cases.zip -d content/en/docs/
-for dir in $(find content/en/docs/excel-test-cases/ -mindepth 1 -maxdepth 1 -type d);
+for dir in $(find content/en/docs/erigrid2-test-cases/ -mindepth 1 -maxdepth 1 -type d);
 do
   cp -a $dir content/en/docs/UseCases
 done
 rm Bridge.zip
-rm -rf content/en/docs/excel-test-cases
+rm -rf content/en/docs/erigrid2-test-cases
